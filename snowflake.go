b520b62580920ea6e685756c4172ccf7e1f5cb41
haibin peng <exiaojiu@163.com>
2018-05-10 11:20:41 +0800

    修复取值范围

--- snowflake.go
+++ snowflake.go
@@ -3,12 +3,15 @@ package snowflake
 import (
 	"time"
 	"sync"
+	"sync/atomic"
 )
 
 const (
 	maxint64	= 0x7fffffffffffffff
+	maxint42	= 0x1ffffffffff
 	max5bit		= 0x1f
 	max12bit	= 0xfff
+	starttime	= 1525397999234
 )
 
 var (
@@ -21,29 +24,40 @@ func GetSnowFlakeID(mid1, mid2 int64) int64 {
 	lock.Lock()
 	defer lock.Unlock()
 	
-	if lastSF >= max12bit {
-		lastSF = 0
-	}
-	
 	newTimeMil := getTimeMill()
-	if newTimeMil <= lastTimeMil {
+	if newTimeMil < lastTimeMil {
 		time.Sleep(time.Duration(lastTimeMil-newTimeMil) + time.Millisecond)
 		newTimeMil = getTimeMill()
 	}
+	// use the same machine code on the same machine
+	if newTimeMil == lastTimeMil && atomic.LoadInt64(&lastSF) > max12bit {
+		time.Sleep(time.Duration(lastTimeMil-newTimeMil) + time.Millisecond)
+	}
+	if newTimeMil > lastTimeMil {
+		atomic.StoreInt64(&lastSF, 0)
+	}
+	
 	lastTimeMil = newTimeMil
-	sf1 := lastTimeMil << 22
+	
+	if (lastTimeMil-starttime) > maxint42 {
+		panic("out of range")
+	}
+	sf1 := (lastTimeMil-starttime) << 22
 	
 	if mid1 > max5bit || mid1 < 0 || mid2 > max5bit || mid2 < 0 {
 		panic("out of range")
 	}
 	sf21 := mid1 << 17
 	sf22 := mid2 << 12
-	
-	lastSF += 1
 	sf3 := lastSF
 	
 	result := maxint64&sf1|sf21|sf22|sf3
 	
+	if result > maxint64 {
+		panic("out of range")
+	}
+	
+	atomic.AddInt64(&lastSF, 1)
 	return result
 }
 
--- snowflake_test.go
+++ snowflake_test.go
@@ -13,16 +13,16 @@ func TestGetSnowFlakeID(t *testing.T) {
 	var m sync.Map
 	wg.Add(4)
 	for gs := 0; gs < 4; gs++ {
-		go func() {
+		go func(gi int) {
 			for i := 0; i < 60; i++ {
 				id := GetSnowFlakeID(10, 10)
-				_, load := m.LoadOrStore(id, i)
+				_, load := m.LoadOrStore(id, struct{}{})
 				if load {
 					t.Fatalf("Repeated ID: ", id)
 				}
 			}
 			wg.Done()
-		}()
+		}(gs)
 	}
 	
 	wg.Wait()
@@ -31,7 +31,7 @@ func TestGetSnowFlakeID(t *testing.T) {
 		i++
 		return true
 	})
-	
+	t.Log(i)
 	if i != 240 {
 		t.Errorf("expected map length: %d", i)
 	}
